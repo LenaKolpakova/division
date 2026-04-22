64c20ba9d444a61b5c7d07acce40572830688ac9
Mumin178 <muminemirov@icloud.com>
2026-04-22 15:46:09 +0300

    first commit

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -4,8 +4,8 @@ test("division",()=>{
     expect(division(4,2)).toEqual(2);
     expect(division(10,2)).toEqual(5);
 
-    expect(division(0,0)).toEqual(Error("неопределенно"));
-    expect(division(6,0)).toEqual(Error("на 0 делить нельзя"));
+    expect(division(0,0)).toEqual(0);
+    expect(division(6,0)).toEqual(0);
     
     expect(division(-6,2)).toEqual(-3);
     expect(division(16,-2)).toEqual(-8);
